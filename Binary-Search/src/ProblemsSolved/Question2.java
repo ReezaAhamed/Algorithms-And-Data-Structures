b62da740b2c728409fc940f7d6b91093fac8faa4
Reeza Ahamed <riiza.ahamed@gmail.com>
2022-01-11 15:27:11 +0100

    Floor of a number

--- Binary-Search/src/ProblemsSolved/Question2.java
+++ Binary-Search/src/ProblemsSolved/Question2.java
@@ -11,13 +11,13 @@ public class Question2 {
 		int[] array = {-15, -12, -3, 0, 1, 3, 7, 15, 17, 21, 35, 55, 99};
 		int[] array2 = {99, 55, 35, 21, 17, 15, 7, 3, 1, 0, -3, -12, -15};
 		int target = 20;
-		int answer = ceilingOfANumber(array, target);
-		int answer2 = ceilingOfANumber(array2, target);
+		int answer = floorOfANumber(array, target);
+		int answer2 = floorOfANumber(array2, target);
 		System.out.println(answer);
 		System.out.println(answer2);
 	}
 	
-	static int ceilingOfANumber(int[] array, int target) {
+	static int floorOfANumber(int[] array, int target) {
 		
 		int start = 0;
 		int end = array.length - 1;
